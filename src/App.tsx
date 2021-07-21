25e8369571de7f17d1eeb6a16cfcba61924e2226
Owen Devine <owen@devine.dev>
2021-07-21 10:28:18 -0400

    React-Static initialization

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Root, Routes, addPrefetchExcludes } from 'react-static'
+import { Router } from '@reach/router'
+import FancyDiv from 'components/FancyDiv'
+import Dynamic from 'containers/Dynamic'
+import './app.css'
+
+// Any routes that start with 'dynamic' will be treated as non-static routes
+addPrefetchExcludes(['dynamic'])
+
+function App() {
+  return (
+    <Root>
+      <div className="content">
+        <FancyDiv>
+          <React.Suspense fallback={<em>Loading...</em>}>
+            <Router>
+              <Dynamic path="dynamic" />
+              <Routes path="*" />
+            </Router>
+          </React.Suspense>
+        </FancyDiv>
+      </div>
+    </Root>
+  )
+}
+
+export default App
